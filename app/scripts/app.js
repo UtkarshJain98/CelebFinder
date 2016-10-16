d77914b7fea167cfb0b8120176a6058021b459b4
nisargkolhe <nisargkolhe@gmail.com>
2016-10-15 23:26:56 -0400

    Implemented Microsoft API

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -34,7 +34,20 @@
 		$http.post('http://nisarg.me:3000/testFormData/', body).
 				success(function(data, status) {
 					if (data.success == true) {
-						console.log("success "+data);
+						console.log(data.filename);
+
+						var mData = {'url': 'http://nisarg.me:3000/uploads/'+data.filename};
+						$http.post('https://api.projectoxford.ai/vision/v1.0/analyze?details=Celebrities', mData, {
+					        headers: { 'Content-Type': 'application/json','Ocp-Apim-Subscription-Key': '92c7f770b37a4349ac0bb04120a1672b'},
+					    })
+					    .success(function(data) {
+					        //do stuff with response
+					        console.log(data);
+					    })
+					    .error(function(data, status, headers, config) {
+							console.log(data);	
+						});
+					
 					} else {
 						console.log(data);
 					}
